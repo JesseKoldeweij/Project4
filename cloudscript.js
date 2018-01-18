710a8adcf832e804314189f85b2d436cb8286b3f
JesseKoldeweij <33252014+JesseKoldeweij@users.noreply.github.com>
2018-01-18 09:04:14 +0100

    Add files via upload

---
+++ cloudscript.js
@@ -0,0 +1,23 @@
+function Geel_1_Jaar(){
+	alert("48 euro");
+} 
+
+function Geel_5_Jaar(){
+	alert("240 euro");
+}
+
+function Oranje_1_Jaar(){
+	alert("72 euro");
+}
+
+function Oranje_5_Jaar(){
+	alert("360 euro");
+}
+
+function Groen_1_Jaar(){
+	alert("120 euro");
+}
+
+function Groen_5_Jaar(){
+	alert("600 euro");
+}
---
+++ hostingscript.js
@@ -0,0 +1,25 @@
+function Brons_1_Jaar(){
+	alert("60 euro");
+} 
+
+function Brons_5_Jaar(){
+	alert("300 euro");
+}
+
+function Silver_1_Jaar(){
+	alert("84 euro");
+}
+
+function Silver_5_Jaar(){
+	alert("475 euro");
+}
+
+function Goud_1_Jaar(){
+	alert("131,40 euro");
+}
+
+function Goud_5_Jaar(){
+	alert("655 euro");
+}
+
+
